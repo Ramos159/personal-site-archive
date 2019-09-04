507d06e4eae27bf9f2857e50dbd42da0794b4675
Ramos159 <edwinramos269@gmail.com>
2019-09-04 15:21:14 -0400

    skills photos, updated about section,slight changes to css, added blog and github links to projects and blogs tab

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,8 +4,15 @@ import Navbar from "../components/navbar.js"
 import styles from "../styles/index.module.css"
 import defaultPic from "../media/defaultpic.gif"
 import Background from "../components/background.js"
-
-
+import CSSPic from "../media/skills/css.png"
+import HTMLPic from "../media/skills/html5.svg"
+import JavaPic from "../media/skills/java.jpg"
+import JSPic from "../media/skills/js.png"
+import ReactPic from "../media/skills/react.png"
+import RubyPic from "../media/skills/ruby.png"
+import RORPic from "../media/skills/rubyonrails.png"
+import SQLPic from "../media/skills/sql.png"
+import WebdevPic from "../media/skills/webdev.png"
 
 export default () =>
 <div>
@@ -16,7 +23,7 @@ export default () =>
     <p className={styles.aboutText}>Often, you there are people who have too much fun... or work too hard...</p>
     <p className={styles.aboutText}>If I didn't work hard, you'd see a rather boring site (god, i hope this isnt boring)</p>
     <p className={styles.aboutText}>I can not guarantee I had fun figuring out the CSS, but i had fun doing other things</p>
-    <p className={styles.aboutText}>A main principle in design tells that more is less. Oddly enough that works for code too</p>
+    <p className={styles.aboutText}>A main principle in design tells that less is more. Oddly enough that works for code too</p>
     <br/>
     <br/>
     <p className={styles.aboutText}>I use my background as a former CS student, and a Flatiron School graduate to produce the good stuff</p>
@@ -25,7 +32,15 @@ export default () =>
     <p className={styles.aboutText}>Whenever I'm not coding, I love playing video games, exploring the city I call home and finding the perfect pizza slice</p>
     <Background/>
     <Footer/>
-    <div className={styles.skillsDiv}>
-        {/* <img></img> */}
+    <div className={styles.flexContainer}>
+        <a href="https://en.wikipedia.org/wiki/JavaScript" target="_blank" rel="noopener noreferrer" alt="Javascript Wiki"><img alt="Javascipt pic" className={styles.icon}src={JSPic}/></a>
+        <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer" alt="React Website"><img alt="React Pic"className={styles.icon}src={ReactPic}/></a>
+        <a href="https://html.spec.whatwg.org/" target="_blank" rel="noopener noreferrer" alt="HTML5 org website"><img alt="HTML Pic"className={styles.icon}src={HTMLPic}/></a>
+        <a href="https://www.oracle.com/java/" target="_blank" rel="noopener noreferrer" alt="Oracle Java website"><img alt="Java Pic"className={styles.icon}src={JavaPic}/></a>
+        <a href="https://www.ruby-lang.org/en/" target="_blank" rel="noopener noreferrer" alt="Official Ruby website"><img alt="Ruby Pic"className={styles.icon}src={RubyPic}/></a>
+        <a href="https://rubyonrails.org/" target="_blank" rel="noopener noreferrer" alt="Ruby on Rails website"><img alt="Ruby on Rails pic"className={styles.icon}src={RORPic}/></a>
+        <a href="https://www.w3.org/Style/CSS/" target="_blank" rel="noopener noreferrer" alt="W3 CSS website"><img alt="CSS Pic"className={styles.icon}src={CSSPic}/></a>
+        <a href="https://en.wikipedia.org/wiki/SQL" target="_blank" rel="noopener noreferrer" alt="SQL Wiki"><img alt="SQL Pic"className={styles.icon}src={SQLPic}/></a>
+        <a href="https://en.wikipedia.org/wiki/Web_development" target="_blank" rel="noopener noreferrer" alt="Web Development Wiki"><img alt="Web Development pic"className={styles.icon}src={WebdevPic}/></a>
     </div>
 </div>
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -12,8 +12,8 @@ export default () =>
 <div>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <Navbar/>
-    <a href={resumePDF} style={{textDecoration:"none"}}ownload="Edwin_Ramos_Resume"><button className={styles.btn}><i className="fa fa-download"></i> Download</button></a>
-    <img className={styles.resume}src={resume}/>
+    <a href={resumePDF} style={{textDecoration:"none"}} download="Edwin_Ramos_Resume"><button className={styles.btn}><i className="fa fa-download"></i> Download</button></a>
+    <img className={styles.resume}src={resume} alt="resume"/>
     <Background/>
     <Footer/>
 </div>
