d8d6e48d965d98b7ed29ed86559fe48eaa4c3523
Ramos159 <edwinramos269@gmail.com>
2019-09-10 13:21:22 -0400

    updated about

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from "react"
 import Footer from "../components/footer.js"
 import Navbar from "../components/navbar.js"
 import styles from "../styles/index.module.css"
-import defaultPic from "../media/defaultpic.gif"
+import defaultPic from "../media/avatar.png"
 import Background from "../components/background.js"
 import CSSPic from "../media/skills/css.png"
 import HTMLPic from "../media/skills/html5.svg"
@@ -23,8 +23,8 @@ export default () =>
     <p className={styles.aboutText}>I like to have fun with things, but also know the value of hard work</p>
     <p className={styles.aboutText}>Often, you find people who either have too much fun or work too hard</p>
     <p className={styles.aboutText}>If I didn't work hard, you'd see a rather boring site to represent me to you </p>
-    <p className={styles.aboutText}>If I didn't have fun, you'd see myself here in a selfie that is way too cropped in </p>
-    <p className={styles.aboutText}>This gif photo is probably better than anything i had in my phone while making this</p>
+    <p className={styles.aboutText}>If I didn't have fun, You wouldn't see this well kept mustache on a default photo  </p>
+    {/* <p className={styles.aboutText}>This gif photo is probably better than anything i had in my phone while making this</p> */}
     <p className={styles.aboutText}>My design was part inspired of sheer annoyance with people over designing their sites </p>
     <p className={styles.aboutText}>A main principle in design tells that less is really more. Oddly enough that works for code too</p>
     <br/>
