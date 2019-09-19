31a8b2dd5d2af2889cbf3dd0cc8796cd42969b97
Ramos159 <edwinramos269@gmail.com>
2019-09-19 06:49:50 -0400

    starting responsive design for mobile, hamburger icon imported

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,18 +1,35 @@
 import React from "react"
 import styles from "../styles/index.module.css"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
-import {Link} from "gatsby"
+import menuIcon from "../media/white-menu.png"
+// import {Link} from "gatsby"
 
 export default class Navbar extends React.Component{
+
+  //  myFunction(){
+  //   let x = document.getElementById(`${styles.navbar}`);
+  //   if (x.className === `${styles.navbar}`) {
+  //     x.className += `${styles.responsive}`;
+  //   } else {
+  //     x.className = `${styles.navbar}`;
+  //   }
+  // }
+
+  showMenu(){
+    return(
+      <ul></ul>
+    )
+  }
     
     render(){
-    return<ul id={styles.navbar}>
+    return<div id={styles.navbar}>
             <li className={styles.navbarItem}><AniLink fade className={styles.navbarLink}to="/">Home</AniLink></li>
             <li className={styles.navbarItem}><a fade className={styles.navbarLink} target="_blank" rel="noopener noreferrer"href="https://medium.com/@edwinramos269">Blogs</a></li>
             <li className={styles.navbarItem}><AniLink fade className={styles.navbarLink}to="/projects">Projects</AniLink></li>
             <li className={styles.navbarItem}><AniLink fade className={styles.navbarLink}to="/resume">Resume</AniLink></li>
             <li className={styles.navbarItemRight}><a className={styles.navbarLink}href="mailto:edwinramos269@gmail.com?subject=Inquiry%20about%20your%20awesomeness&body=Hey%20Edwin,%20I%20was%20wondering%20how%20you%20became%20so%20awesome...">Contact</a></li>
             <li className={styles.navbarItemRight}><AniLink fade className={styles.navbarLink}to="/about">About</AniLink></li>
-          </ul>
+            <li className={styles.navbarItemRight}><img onClick={this.showMenu}className={styles.hiddenIcon}src={menuIcon}/></li>
+          </div>
     }
 }
