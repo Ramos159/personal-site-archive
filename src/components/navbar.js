158f7a7cf6d399b6d088d377b77a3752e90308fe
Ramos159 <edwinramos269@gmail.com>
2019-10-01 09:43:22 -0400

    button change

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -14,15 +14,36 @@ export default class Navbar extends React.Component{
   //     x.className = `${styles.navbar}`;
   //   }
   // }
+  state = {
+    toggle:false
+  }
 
-  showMenu(){
+  showMenu = () => {
+    console.log("THIS")
+    let menu = document.getElementById(`${styles.navbar}`)
+    if(menu.className == "holder"){
+      menu.className += "responsive"
+    }
+    else{
+      menu.className = "holder"
+    }
+  }
+
+  responsiveMenu = () =>{
     return(
-      <ul></ul>
+      <React.Fragment>
+      <li className={styles.responsiveItem}><AniLink fade className={styles.navbarLink}to="/">Home</AniLink></li>
+      <li className={styles.responsiveItem}><a fade className={styles.navbarLink} target="_blank" rel="noopener noreferrer"href="https://medium.com/@edwinramos269">Blogs</a></li>
+      <li className={styles.responsiveItem}><AniLink fade className={styles.navbarLink}to="/projects">Projects</AniLink></li>
+      <li className={styles.responsiveItem}><AniLink fade className={styles.navbarLink}to="/resume">Resume</AniLink></li>
+      <li className={styles.responsiveItem}><a className={styles.navbarLink}href="mailto:edwinramos269@gmail.com?subject=Inquiry%20about%20your%20awesomeness&body=Hey%20Edwin,%20I%20was%20wondering%20how%20you%20became%20so%20awesome...">Contact</a></li>
+      <li className={styles.responsiveItem}><AniLink fade className={styles.navbarLink}to="/about">About</AniLink></li>
+      </React.Fragment>
     )
   }
     
     render(){
-    return<div id={styles.navbar}>
+    return<div className="holder"id={styles.navbar}>
             <li className={styles.navbarItem}><AniLink fade className={styles.navbarLink}to="/">Home</AniLink></li>
             <li className={styles.navbarItem}><a fade className={styles.navbarLink} target="_blank" rel="noopener noreferrer"href="https://medium.com/@edwinramos269">Blogs</a></li>
             <li className={styles.navbarItem}><AniLink fade className={styles.navbarLink}to="/projects">Projects</AniLink></li>
@@ -30,6 +51,14 @@ export default class Navbar extends React.Component{
             <li className={styles.navbarItemRight}><a className={styles.navbarLink}href="mailto:edwinramos269@gmail.com?subject=Inquiry%20about%20your%20awesomeness&body=Hey%20Edwin,%20I%20was%20wondering%20how%20you%20became%20so%20awesome...">Contact</a></li>
             <li className={styles.navbarItemRight}><AniLink fade className={styles.navbarLink}to="/about">About</AniLink></li>
             <li className={styles.navbarItemRight}><img onClick={this.showMenu}className={styles.hiddenIcon}src={menuIcon}/></li>
+            {/* {this.state.toggle  ? <React.Fragment>
+                                    <li className={styles.responsiveItem}><AniLink fade className={styles.navbarLink}to="/">Home</AniLink></li>
+                                    <li className={styles.responsiveItem}><a fade className={styles.navbarLink} target="_blank" rel="noopener noreferrer"href="https://medium.com/@edwinramos269">Blogs</a></li>
+                                    <li className={styles.responsiveItem}><AniLink fade className={styles.navbarLink}to="/projects">Projects</AniLink></li>
+                                    <li className={styles.responsiveItem}><AniLink fade className={styles.navbarLink}to="/resume">Resume</AniLink></li>
+                                    <li className={styles.responsiveItem}><a className={styles.navbarLink}href="mailto:edwinramos269@gmail.com?subject=Inquiry%20about%20your%20awesomeness&body=Hey%20Edwin,%20I%20was%20wondering%20how%20you%20became%20so%20awesome...">Contact</a></li>
+                                    <li className={styles.responsiveItem}><AniLink fade className={styles.navbarLink}to="/about">About</AniLink></li>
+                                  </React.Fragment> : null} */}
           </div>
     }
 }
