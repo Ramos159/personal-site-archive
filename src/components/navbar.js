a204e4c2390803eb9e1b2eda032dfc87e14843c8
Ramos159 <edwinramos269@gmail.com>
2020-05-11 04:51:36 -0400

    fixed links and resize issue

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,6 +2,7 @@ import React from "react"
 import "../styles/index.css"
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class CustomNav extends React.Component{
@@ -45,22 +46,16 @@ export default class CustomNav extends React.Component{
   // <a href={null} className="hiddenIcon" onClick={this.showMenu}><img id="burger"src={menuIcon}/></a>
     
     render(){
-  //   return<Navbar bg="light" expand="lg">
-  //     <Nav className="mr-auto">
-  //       <Nav.Link href="#home">Home</Nav.Link>
-  //       <Nav.Link href="#link">Link</Nav.Link>
-  //     </Nav>
-  // </Navbar>
   return<Navbar bg="black" expand="lg" variant="dark">
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="https://www.edwinjramos.com/">Home</Nav.Link>
-      <Nav.Link href="https://medium.com/@edwinramos269">Blogs</Nav.Link>
-      <Nav.Link href="https://www.edwinjramos.com/projects">Projects</Nav.Link>
-      <Nav.Link href="https://www.edwinjramos.com/resume">Resume</Nav.Link>
-      <Nav.Link href="mailto:edwinramos269@gmail.com?subject=Inquiry%20about%20your%20awesomeness&body=Hey%20Edwin,%20I%20was%20wondering%20how%20you%20became%20so%20awesome...">Contact</Nav.Link>
-      <Nav.Link href="https://www.edwinjramos.com/about">About</Nav.Link>
+        <AniLink fade to="/">Home</AniLink>
+        <a fade  target="_blank" rel="noopener noreferrer"href="https://medium.com/@edwinramos269">Blogs</a>
+        <AniLink fade to="/projects">Projects</AniLink>
+        <AniLink fade to="/resume">Resume</AniLink>
+        <a href="mailto:edwinramos269@gmail.com?subject=Inquiry%20about%20your%20awesomeness&body=Hey%20Edwin,%20I%20was%20wondering%20how%20you%20became%20so%20awesome...">Contact</a>
+        <AniLink fade to="/about">About</AniLink>
       {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
