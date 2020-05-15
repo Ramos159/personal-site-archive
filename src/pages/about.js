dca71bbc01ffbc5b4aebb0c721ed7e8830f9ba78
Ramos159 <edwinramos269@gmail.com>
2020-05-15 04:23:16 -0400

    icon css change, moved around skill pics + added TS

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,6 +13,7 @@ import RubyPic from "../media/skills/ruby.png"
 import RORPic from "../media/skills/rubyonrails.png"
 import PYPic from "../media/skills/python.ico"
 import WebdevPic from "../media/skills/webdev.png"
+import TSPic from "../media/skills/typescript.svg"
 
 export default () =>
 <div style={{  position: "relative",
@@ -35,15 +36,16 @@ export default () =>
     <p className='aboutText'>Whenever I'm not coding, I love playing video games, exploring the city I call home and finding the perfect pizza slice</p>
     <Background/>
     <div className='flexContainer'>
+        <a href="https://www.typescriptlang.org/" target="_blank" rel="noopenner noreferrer" alt="Typescript Website"><img alt="Typescript Pic" className='icon' src={TSPic}/></a>
         <a href="https://en.wikipedia.org/wiki/JavaScript" target="_blank" rel="noopener noreferrer" alt="Javascript Wiki"><img alt="Javascipt pic" className='icon'src={JSPic}/></a>
         <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer" alt="React Website"><img alt="React Pic"className='icon'src={ReactPic}/></a>
         <a href="https://html.spec.whatwg.org/" target="_blank" rel="noopener noreferrer" alt="HTML5 org website"><img alt="HTML Pic"className='icon'src={HTMLPic}/></a>
-        <a href="https://www.oracle.com/java/" target="_blank" rel="noopener noreferrer" alt="Oracle Java website"><img alt="Java Pic"className='icon'src={JavaPic}/></a>
+        <a href="https://www.w3.org/Style/CSS/" target="_blank" rel="noopener noreferrer" alt="W3 CSS website"><img alt="CSS Pic"className='icon'src={CSSPic}/></a>
+        <a href="https://en.wikipedia.org/wiki/Web_development" target="_blank" rel="noopener noreferrer" alt="Web Development Wiki"><img alt="Web Development pic"className='icon'src={WebdevPic}/></a>
+        <a href="https://python.org" target="_blank" rel="noopener noreferrer" alt="Python Wiki"><img alt="Pyhton Pic"className='icon'src={PYPic}/></a>
         <a href="https://www.ruby-lang.org/en/" target="_blank" rel="noopener noreferrer" alt="Official Ruby website"><img alt="Ruby Pic"className='icon'src={RubyPic}/></a>
         <a href="https://rubyonrails.org/" target="_blank" rel="noopener noreferrer" alt="Ruby on Rails website"><img alt="Ruby on Rails pic"className='icon'src={RORPic}/></a>
-        <a href="https://www.w3.org/Style/CSS/" target="_blank" rel="noopener noreferrer" alt="W3 CSS website"><img alt="CSS Pic"className='icon'src={CSSPic}/></a>
-        <a href="https://python.org" target="_blank" rel="noopener noreferrer" alt="Python Wiki"><img alt="Pyhton Pic"className='icon'src={PYPic}/></a>
-        <a href="https://en.wikipedia.org/wiki/Web_development" target="_blank" rel="noopener noreferrer" alt="Web Development Wiki"><img alt="Web Development pic"className='icon'src={WebdevPic}/></a>
+        <a href="https://www.oracle.com/java/" target="_blank" rel="noopener noreferrer" alt="Oracle Java website"><img alt="Java Pic"className='icon'src={JavaPic}/></a>
     </div>
     <Footer/>
 </div>
