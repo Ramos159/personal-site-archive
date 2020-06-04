c3180db8427460ae661b1c07a906dece51ade82c
Ramos159 <edwinramos269@gmail.com>
2020-06-04 05:25:19 -0400

    okay looking github cards for now

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -15,15 +15,15 @@ export default () =>
     <h1 className='indexTitle'>Projects</h1>
     <h2 className='indexTitle'>This Website!</h2>
     <div id='project'>
-    <a href="https://github.com/Ramos159/personal-site"><img src="https://gh-card.dev/repos/Ramos159/personal-site.svg"/></a>
+      <a href="https://github.com/Ramos159/personal-site"><img src="https://gh-card.dev/repos/Ramos159/personal-site.svg"/></a>
     </div>
     <p className='indexTitle'>Not much to explain here, just this website. 100% handbuilt</p>
     <p className='indexTitle'>Nice looking Web Pages and Animations with plain HTML and CSS(hopefully)</p>
     <p className='indexTitle'>Built with Javascript, Gatsby.Js and Netlify</p>
     <hr style={{color:'white',backgroundColor:'white',width:'40%'}}></hr>
-    {/* <h2 className='indexTitle'>PocketDex</h2>
+    <h2 className='indexTitle'>PocketDex</h2>
     <div id='project'>
-      <RepoCard username='Ramos159' repo='Pokemon-react-native-app'/>
+      <a href="https://github.com/Ramos159/Pokemon-React-Native-App"><img src="https://gh-card.dev/repos/Ramos159/Pokemon-React-Native-App.svg"/></a>
     </div>
     <p className='indexTitle'>This is a work in-progress and my first forray into mobile development.</p>
     <p className='indexTitle'>Will emulate a Pokedex from the Pokemon Series. Great source on everything Pokeverse</p>
@@ -32,8 +32,8 @@ export default () =>
     <hr style={{color:'white',backgroundColor:'white',width:'40%'}}></hr>
     <a style={{color:"white",textDecoration:"underline"}} rel="noopener noreferrer" target="_blank" href="https://nycttp-frontend.herokuapp.com/"><h2 className='indexTitle'>StockFolio</h2></a>
     <div id='project'>
-      <RepoCard username='Ramos159' repo='reactdex-backend'/>
-      <RepoCard username='dankoMong' repo='reactdex-frontend'/>
+      <a href="https://github.com/Ramos159/NYC-TTP-frontend"><img src="https://gh-card.dev/repos/Ramos159/NYC-TTP-frontend.svg"/></a>
+      <a href="https://github.com/Ramos159/NYC-TTP-backend"><img src="https://gh-card.dev/repos/Ramos159/NYC-TTP-backend.svg"/></a>
     </div>
     <p className='indexTitle'>Web App Project made for NYC Tech Talent Pipeline Assesment</p>
     <p className='indexTitle'>Real time stock data with Alphavantage API, encrypted login with BCrypt, Semantic UI components</p>
@@ -42,8 +42,8 @@ export default () =>
     <hr style={{color:'white',backgroundColor:'white',width:'40%'}}></hr>
     <a style={{color:"white",textDecoration:"underline"}} rel="noopener noreferrer" target="_blank" href="https://event-hub-site.herokuapp.com/"><h2 className='indexTitle'>EventHub</h2></a>
     <div id='project'>
-      <RepoCard username='Ramos159' repo='eventhub-frontend'/>
-      <RepoCard username='Ramos159' repo='eventhub-backend'/>
+      <a href="https://github.com/Ramos159/eventhub-frontend"><img src="https://gh-card.dev/repos/Ramos159/eventhub-frontend.svg"/></a>
+      <a href="https://github.com/Ramos159/eventhub-backend"><img src="https://gh-card.dev/repos/Ramos159/eventhub-backend.svg"/></a>
     </div>
     <p className='indexTitle'>Web App that emulates an ecommerce site based on selling tickets to events</p>
     <p className='indexTitle'>Uses 2 TicketMaster API's, BCrypt, Postgres, Semantic UI</p>
@@ -52,8 +52,8 @@ export default () =>
     <hr style={{color:'white',backgroundColor:'white',width:'40%'}}></hr>
     <a style={{color:"white",textDecoration:"underline"}} rel="noopener noreferrer" target="_blank" href="https://www.youtube.com/watch?v=3cc1s6kSAmM"><h2 className='indexTitle'>ReactDex</h2></a>
     <div id='project'>
-      <RepoCard username='Ramos159' repo='reactdex-backend'/>
-      <RepoCard username='dankoMong' repo='reactdex-frontend'/>
+      <a href="https://github.com/dankomong/reactdex-frontend"><img src="https://gh-card.dev/repos/dankomong/reactdex-frontend.svg"/></a>
+      <a href="https://github.com/Ramos159/reactdex-backend"><img src="https://gh-card.dev/repos/Ramos159/reactdex-backend.svg"/></a>
     </div>
     <p className='indexTitle'>Pokemon Themed Web App that revolves around building teams</p>
     <p className='indexTitle'>Uses Official Pokemon API, BCrypt, MySql</p>
@@ -61,11 +61,11 @@ export default () =>
     <hr style={{color:'white',backgroundColor:'white',width:'40%'}}></hr>
     <h2 className='indexTitle'>Geometric Warfare</h2>
     <div id='project'>
-      <RepoCard username='Ramos159' repo='geometric-warfare'/>
+    <a href="https://github.com/Ramos159/personal-site"><img src="https://gh-card.dev/repos/Ramos159/personal-site.svg"/></a>
     </div>
     <p className='indexTitle'>Browser game app inspired by Geometry wars</p>
     <p className='indexTitle'>Classic spaceship shooter game with scaling difficulty and humorous game assets</p>
-    <p className='indexTitle' style={{marginBottom:'50px'}}>Built with Javascript, Phaser3.JS Game Library, Ruby, Ruby on Rails</p> */}
+    <p className='indexTitle' style={{marginBottom:'50px'}}>Built with Javascript, Phaser3.JS Game Library, Ruby, Ruby on Rails</p>
     <Background/>
     <Footer/>
 </div>
